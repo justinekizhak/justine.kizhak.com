2075304cd17652e558423b9af41aeb9b1ded7449
Justine Kizhakkinedath <justine@kizhak.com>
2019-10-26 18:16:44 +0530

    Add link.svelte

---
+++ src/components/nav/Link.svelte
@@ -0,0 +1,5 @@
+<div class="text-center justify-center text-main bg-white hover:shadow-main hover:text-white hover:bg-main">
+    <a href="#" class="w-24 h-8 block no-underline leading-loose hover:text-white">
+        <slot></slot>
+    </a>
+</div>
