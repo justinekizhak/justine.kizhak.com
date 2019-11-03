fec52d7da3f72f5feb5b6277f94976a0d3e11caf
Justine Kizhakkinedath <justine@kizhak.com>
2019-11-03 22:42:25 +0530

    Update contact page

--- src/routes/contact-me.svelte
+++ src/routes/contact-me.svelte
@@ -2,12 +2,46 @@
  import ContentLayout from './_content_layout.svelte';
 </script>
 
+<style>
+ input, textarea {
+     @apply w-full h-8 my-4 bg-gray-400;
+ }
+ textarea {
+     @apply h-40;
+ }
+</style>
+
 <svelte:head>
 	<title>Contact | Justine kizhakkinedath</title>
 </svelte:head>
 
 <ContentLayout>
-    <h1>Contact this site</h1>
+    <h1>Contact Me</h1>
 
-    <p>This is the 'contact' page. There's not much here.</p>
+    <form class="text-base" name="contact" method="POST" netlify-honeypot="bot-field"
+          data-netlify="true" data-netlify-recaptcha="true">
+        <p class="hidden">
+            <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+        </p>
+        <p>
+            <label>Your Name
+            <input type="text" name="name" />
+            </label>
+        </p>
+        <p>
+            <label>Your Email
+                <input type="email" name="email" />
+            </label>
+        </p>
+        <p>
+            <label>Message
+                <textarea name="message"></textarea>
+            </label>
+        </p>
+        <div data-netlify-recaptcha="true"></div>
+        <div class="bg-main w-32 h-8 hover:shadow-main text-white">
+            <button class="w-32 h-8" type="submit">Send</button>
+        </div>
+    </form>
+
 </ContentLayout>
